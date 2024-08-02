831ad9948d14b2254de8f12c9b9ef2e3a91394b2
Shen Kai <shengkaicao60372@gmail.com>
2024-08-02 16:23:01 +0800

    調整

--- file.py
+++ file.py
@@ -1,18 +1,29 @@
 # 儲存檔案
 # w 檔案的寫入
 
-file = open("data.txt", mode="w", encoding="utf-8") # 開啟
-file.write("Hello world\n") # 操作
-file.write("測試中文\n好棒棒") # 中文操作
-file.close()
+# file = open("data.txt", mode="w", encoding="utf-8") # 開啟
+# file.write("Hello world\n") # 操作
+# file.write("測試中文\n好棒棒") # 中文操作
+# file.close()
 
-with open("data2.txt", mode = "w", encoding="utf-8") as file:
-    file.write("測試新寫法\nRRRRRRR")
+with open("data.txt", mode = "w", encoding="utf-8") as file:
+    file.write("1\n")
+    file.write("3\n")
+    file.write("2\n")
 
-# 讀取檔案
+# # r 檔案的讀取
+# with open("data.txt", mode = "r", encoding="utf-8") as file:
+#     date =  file.read()
+# print(date)
+
+# r 檔案的讀取
+sum = 0
 with open("data.txt", mode = "r", encoding="utf-8") as file:
-    date =  file.read()
-print(date)
+    for line in file:
+      sum += int(line)
+
+    # date =  file.read() 整個文件寫出
+print(sum)
 
 
 
