853a72f16ac40f3c970cdeb44c45a61c01122a62
Shen Kai <shengkaicao60372@gmail.com>
2024-08-15 13:11:34 +0800

    練習

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 # 主程式
 
 import gemometry.point
-n1 = input("請輸入數值:")
+#n1 = input("請輸入數值:")
 result = gemometry.point.distance(3,4,1,2)
 print(result)
 
@@ -9,3 +9,13 @@ print("----------------------------------------")
 import gemometry.line as line
 result = line.slope(3,4,1,2)
 print(result)
+
+sum = 0
+n = 8
+for i in range(1,n+1,2):
+    sum += i
+print(type(2.))
+\
+import datetime
+d = datetime.datetime(2024,6,13)
+print('{:%B-%d-%y}'.format(d))
