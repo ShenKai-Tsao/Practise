95bfa852392d5ae122efeede1875c91f95fca87b
Shen Kai <shengkaicao60372@gmail.com>
2024-08-16 14:41:34 +0800

    新增可疊帶練習

---
+++ iterable.py
@@ -0,0 +1,35 @@
+# iterable 資料型態
+
+# 字串、列表、集合、字典
+
+# for 迴圈
+# for 迴圈 IN 可疊代的資料
+
+# 列表
+print("列表")
+for x in ["a","b","c"]:
+    print(x)
+
+# 字串
+print("字串")
+for x in "abc":
+    print(x)
+
+# 集合
+print("集合")
+for x in {"a","c","s"}:
+    print(x)
+
+# 字典
+print("字典")
+dic = {"a":3,"c":4}
+for x in dic:
+    print(x + ":" + str(dic[x]))
+
+# 內建函式
+print("內建函式")
+result = max([10,20,30,40])
+print(result)
+
+result = sorted([10,20,30,40], reverse = True)
+print(result)
